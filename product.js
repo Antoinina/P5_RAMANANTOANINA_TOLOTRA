040030ba57cbfe22831da5a6119f5ee49aa47545
Antoinina <tolotra.ramanantoanina@gmail.com>
2020-07-24 13:23:50 +0200

    Homepage finished + add 404 page

---
+++ product.js
@@ -0,0 +1,16 @@
+class Product{
+    constructor(id, name, price, description, imageUrl, status){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.status = status;
+    }
+}
+
+let vcam1 = new Product ("452", "Compact Zeiss Icon", 259, "Zeiss Icon Symbolica avec Tessar 50 mm f2.8", ": http://localhost:3000/api/cameras", "available";
+let vcam2 = new Product ("123", "Compact Canon", 120.99, "Boîtier + objectif capteur 1/2.3 40mm f5", ": http://localhost:3000/api/cameras", "available";
+let vcam3 = new Product ("652", "Ancien Vario", 620, "Ancien appareil photo très sympa en décoration 530g 35mm", ": http://localhost:3000/api/cameras", "available";
+let vcam4 = new Product ("006", "Compact Olympus", 152, "Olympus OM-D E-M10 MARK III silver+14-42MM Silver+40-150MM noir", ": http://localhost:3000/api/cameras", "available";
+let vcam5 = new Product ("322", "Compact Minolta", 340, "Boîtier nu Correction d'exposition -3 EV 1/3 80mm", ": http://localhost:3000/api/cameras", "available";
